3cbe8bf09f8780771bc3afd1eb3ad138fc986fc5
FireAndy <asvea2003@yahoo.dk>
2017-09-26 13:21:41 +0200

    added h1 intro tekster

--- js/header-carousel.js
+++ js/header-carousel.js
@@ -18,9 +18,10 @@ $(document).ready(function() {
 			$('header')
 				.css('background-image','url("'+ carouselImages[caroIndex] + '")')
 				.fadeIn(1000, function(){setTimeout(carousel,12000);});
+                
+        
 		caroIndex++;
 		caroIndex = caroIndex % carouselImages.length;
 	};
 	carousel();
 });
-
